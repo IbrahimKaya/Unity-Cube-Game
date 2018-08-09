1331df0bb1ceb0b34e344872407c6a8b49a03443
IbrahimKaya <kayaibrahim0@yandex.com>
2018-08-09 21:21:28 +0300

    game core version

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+ 
+    public Rigidbody rb;
+
+        // Use this for initialization
+        public float fowardForce = 2000f;
+        public float sideWaysForce = 500f;
+	    void Start () {
+        // rb.AddForce(0,200,500);
+        
+	    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+       
+       rb.AddForce(0, 0, fowardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideWaysForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideWaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0,fowardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -fowardForce * Time.deltaTime);
+        }
+
+        if (rb.position.y < 0)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+
+    }
+}
